3133c9de8856df20dab6f1378d824cdf39680077
RaymondLoranger <Raymond.Loranger@bell.net>
2018-10-24 11:20:19 -0400

    0.1.1

--- lib/minimal_todo.ex
+++ lib/minimal_todo.ex
@@ -1,27 +1,56 @@
 defmodule MinimalTodo do
+  alias __MODULE__.Spec
   alias IO.ANSI.Table
 
+  @spec start() :: :ok
   def start do
-    "Name of .csv to load: "
-    |> IO.gets()
-    |> String.trim()
-    |> parse_and_format()
+    map_filename()
+    |> parse_csv()
+    |> csv_to_maps()
+    |> map_sort_specs()
+    |> format_maps()
   end
 
-  def parse_and_format(filename) do
+  ## Private functions
+
+  @spec map_filename() :: Spec.t()
+  defp map_filename do
+    "Name of .csv to format: " |> IO.gets() |> String.trim() |> Spec.new()
+  end
+
+  @spec parse_csv(Spec.t()) :: Spec.t()
+  defp parse_csv(%Spec{} = spec) do
     [headers | lines] =
-      for line <- File.stream!(filename) do
+      for line <- File.stream!(spec.filename) do
         line |> String.split(",") |> Enum.map(&String.trim/1)
       end
 
-    Enum.map(lines, fn line ->
-      Enum.zip(headers, line) |> Map.new()
-    end)
-    |> Table.format(
-      headers: headers,
-      sort_specs: Enum.slice(headers, 0..1),
-      style: :light,
-      bell: true
+    %Spec{spec | headers: headers, lines: lines}
+  end
+
+  @spec map_sort_specs(Spec.t()) :: Spec.t()
+  defp map_sort_specs(%Spec{} = spec) do
+    %Spec{spec | sort_specs: Enum.slice(spec.headers, 0..1)}
+  end
+
+  @spec csv_to_maps(Spec.t()) :: Spec.t()
+  defp csv_to_maps(%Spec{} = spec) do
+    maps =
+      Enum.map(spec.lines, fn line ->
+        Enum.zip(spec.headers, line) |> Map.new()
+      end)
+
+    %Spec{spec | maps: maps}
+  end
+
+  @spec format_maps(Spec.t()) :: :ok
+  defp format_maps(%Spec{} = spec) do
+    Table.format(
+      spec.maps,
+      headers: spec.headers,
+      sort_specs: spec.sort_specs,
+      style: spec.style,
+      bell: spec.bell
     )
   end
 end
