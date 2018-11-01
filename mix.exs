9bb8f85f8c2d5b8f5f009bc9eada7907d6f4a92e
RaymondLoranger <Raymond.Loranger@bell.net>
2018-11-01 18:24:40 -0400

    0.1.2

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule AlchemistCamp.MixProject do
   def project do
     [
       app: :alchemist_camp,
-      version: "0.1.1",
+      version: "0.1.2",
       elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
       deps: deps()
